376c0e69eb01aad2059d7c4de1ccb2d7ab25c645
Asher Haig <asher@ridiculouspower.com>
2012-10-24 04:01:12 -0400

    reorganization of file structure

--- lib/perspective-path.rb
+++
@@ -1,2 +0,0 @@
-
-require_relative 'perspective/path.rb'
---
+++ lib/perspective/request/namespaces.rb
@@ -0,0 +1,15 @@
+
+class ::Perspective::Request
+  class Path
+    class RequestPath
+    end
+  	module PathPart
+  		class Optional
+  			class NamedOptional < ::Perspective::Request::Path::PathPart::Optional
+  			end
+  		end
+  		module Fragment
+  	  end
+	  end
+	end
+end
